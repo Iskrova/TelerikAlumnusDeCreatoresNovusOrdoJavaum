5bb859b7f815a88fc7ea3c64469340001b66270a
Iskrova <mary.iskrova@gmail.com>
2018-05-06 22:56:15 +0300

    Some extra code being removed!

--- src/Society/Entered.java
+++ src/Society/Entered.java
@@ -3,17 +3,9 @@ package Society;
 import PlayerProperties.Rank;
 
 public  abstract class Entered extends Coder {
-    private static final int MIN_EXPERIENCE = 300;
-    private static final int MAX_EXPERIENCE = 555;
+
     public Entered(String name) {
         super(name, Rank.ENTERED);
     }
 
-    public static int getMinExperience() {
-        return MIN_EXPERIENCE;
-    }
-
-    public static int getMaxExperience() {
-        return MAX_EXPERIENCE;
-    }
 }

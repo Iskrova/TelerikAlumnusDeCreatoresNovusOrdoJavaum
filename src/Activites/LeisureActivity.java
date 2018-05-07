c383f6cd452ac95569b183a7bfdf1825ea37ed9d
Pepi <petroslav.angelov@gmail.com>
2018-05-07 16:48:24 +0300

    Fixed another null pointer

--- src/Activites/LeisureActivity.java
+++ src/Activites/LeisureActivity.java
@@ -23,22 +23,32 @@ public class LeisureActivity {
     }
     private void goOut(Player player, int energy, int time){
         player.setEnergy(energy + 1);
-        player.getCurrentTask().reduceDeadline(time);
+        if(player.getCurrentTask() != null){
+            player.getCurrentTask().reduceDeadline(time);
+        }
     }
     private void learn(Player player, int energy, int time){
         player.setEnergy(energy + 2);
-        player.getCurrentTask().reduceDeadline(time);
+        if(player.getCurrentTask() != null){
+            player.getCurrentTask().reduceDeadline(time);
+        }
     }
     private void drinkBeer(Player player, int energy, int time){
         player.setEnergy(energy + 3);
-        player.getCurrentTask().reduceDeadline(time);
+        if(player.getCurrentTask() != null){
+            player.getCurrentTask().reduceDeadline(time);
+        }
     }
     private void goToTheBeach(Player player, int energy, int time){
         player.setEnergy(energy + 4);
-        player.getCurrentTask().reduceDeadline(time);
+        if(player.getCurrentTask() != null){
+            player.getCurrentTask().reduceDeadline(time);
+        }
     }
     private void goSightseeing(Player player, int energy, int time){
         player.setEnergy(energy + 5);
-        player.getCurrentTask().reduceDeadline(time);
+        if(player.getCurrentTask() != null){
+            player.getCurrentTask().reduceDeadline(time);
+        }
     }
 }
